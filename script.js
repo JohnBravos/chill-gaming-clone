572e305b6713c81824c404592adcaa82af5384c9
bravosgiannis <bravosgiannis@gmail.com>
2026-06-06 12:27:36 +0300

    refactor: rewrite article utility functions using array methods
    - filterByCategory: for loop → filter
    - searchArticles: for loop → filter
    - countByCategory: for loop → reduce

--- script.js
+++ script.js
@@ -14,38 +14,68 @@ let articles = [
     {title: `Το Μεγάλο REVIEW του 'DOOM: The Dark Ages'`, category: "Gaming Reviews", author: "User", date: "15/10/2025"}
 ];
 
-function filterByCategory(articles, cat) {
-    let result = [];
-    for (let article of articles) {
-        if (article.category.toLowerCase() === cat.toLowerCase()) {
-            result.push(article);
-        }
-    }
-    return result;
+// function filterByCategory(arr, cat) {
+//     let result = [];
+//     for (let article of arr) {
+//         if (article.category.toLowerCase() === cat.toLowerCase()) {
+//             result.push(article);
+//         }
+//     }
+//     return result;
+// }
+
+let filterByCategory = (arr, cat) => {
+    let newArticles = arr.filter(article => {
+        return article.category.toLowerCase() === cat.toLowerCase();
+    })
+    
+    return newArticles;
 }
 
-function countByCategory(articles) {
-    let result = {};
 
-    for (let article of articles) {
+// function countByCategory(arr) {
+//     let result = {};
+
+//     for (let article of arr) {
+//         if (!result[article.category]) {
+//             result[article.category] = 1;
+//         } else {
+//             result[article.category] += 1;
+//         }
+//     }
+    
+//     return result;
+// }
+
+let countByCategory = (arr) => {
+    let newArr = arr.reduce((result, article) => {
         if (!result[article.category]) {
             result[article.category] = 1;
         } else {
             result[article.category] += 1;
         }
-    }
-    
-    return result;
+        return result;
+    }, {})
+
+    return newArr;
 }
 
-function searchArticles(articles, keySearch) {
-    let result = [];
+// function searchArticles(arr, keySearch) {
+//     let result = [];
 
-    for (let article of articles) {
-        if (article.title.toLowerCase().includes(keySearch.toLowerCase())) {
-            result.push(article);
-        }
-    }
+//     for (let article of arr) {
+//         if (article.title.toLowerCase().includes(keySearch.toLowerCase())) {
+//             result.push(article);
+//         }
+//     }
 
-    return result;
+//     return result;
+// }
+
+let searchArticles = (arr, key) => {
+    let newArr = arr.filter(article => article.title.toLowerCase().includes(key.toLowerCase()))
+        
+    return newArr;
 }
+
+console.log(searchArticles(articles, "remedy"));
